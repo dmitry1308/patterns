3f4fdbbf3fa7d7ef791b7b22d697b934a4fdb6d2
Schepin Dmitriy <bright1308@gmail.com>
2021-02-17 08:33:37 +0300

    commit observer pattern

---
+++ src/main/java/shepin/patterns/observer/JobSearch.java
@@ -0,0 +1,21 @@
+package shepin.patterns.observer;
+
+public class JobSearch {
+	public static void main(String[] args) {
+		JavaDeveloperJobSite site = new JavaDeveloperJobSite();
+		site.addVacances("First vacancy");
+		site.addVacances("Second vacancy");
+
+		Observer subscriber1 = new Subscriber("Igor");
+		Observer subscriber2 = new Subscriber("Ivan");
+		Observer subscriber3 = new Subscriber("Dima");
+
+		site.addObserver(subscriber1);
+		site.addObserver(subscriber2);
+		site.addObserver(subscriber3);
+
+		site.addVacances("Trird vacancy");
+		site.removeVacancy("First vacancy");
+
+	}
+}
---
+++ src/main/java/shepin/patterns/observer/Observer.java
@@ -0,0 +1,7 @@
+package shepin.patterns.observer;
+
+import java.util.List;
+
+public interface Observer {
+	public void handleEvent(List<String>vacances);
+}
